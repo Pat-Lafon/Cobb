3a0048475e298b48b7bd20382c602a7bf8400d29
Anxhelo Xhebraj <axhebraj@purdue.edu>
2023-10-23 23:33:04 -0400

    Add initial utility functions

--- bin/main.ml
+++ bin/main.ml
@@ -59,7 +59,9 @@ let source_file =
 let refine_file =
   "underapproximation_type/data/benchmark/quickchick/sizedlist/_under.ml"
 
+(* This sets up global variables pointing to the information in meta-config.json *)
 let () = Env.load_meta meta_config_file
+(* prim.ml:init sets up global maps of the stuff that is loaded from the config *)
 let () = Config.load refine_file
 
 (*** Notations: ... todo *)
@@ -71,11 +73,20 @@ let () = Config.load refine_file
 let notations, libs, refinements =
   Inputstage.load_user_defined_under_refinments refine_file
 
+let dbg_sexp sexp = print_endline (Core.Sexp.to_string_hum sexp)
+let dbg (ut: UT.t) = dbg_sexp (UT.sexp_of_t ut)
+
 let () = print_endline (List.length notations |> string_of_int)
 let () = print_endline (List.length libs |> string_of_int)
-let _ = List.map (fun (x, _) -> print_endline x) libs
+let _ = List.map (fun (name, x) ->  print_endline name ; dbg x) libs
 let () = print_endline (List.length refinements |> string_of_int)
 let _ = List.map (fun (_, (n, _)) -> print_endline n) refinements
+
+(* NOTE: the code below shows what operations are available according 
+  to where the type checker looks *)
+let ops = Abstraction.Prim_map.get_normal_m ()
+let tmp = Abstraction.Prim_map.S.iter (fun op -> fun x -> dbg_sexp (Op.sexp_of_t op)) ops
+(* let _ = exit 0 *)
 let code = Inputstage.load_ssa libs source_file
 
 let nctx =
@@ -88,6 +99,104 @@ let libctx =
   List.fold_left
     (fun ctx (x, ty) -> Nctx.add_to_right ctx (x, ty))
     Nctx.empty libs
+
+let args_is_unit (t: UT.t) =
+  let nty = UT.erase t in
+  let (argtys, resty) = NT.destruct_arrow_tp nty in
+  match argtys with
+  | [Ty_unit] -> true
+  | _ -> false
+
+let seeds = List.filter (fun (name, uty) -> args_is_unit uty) libs
+
+let map_fst f = fun (l, r) -> (f l, r)
+
+let freshen (ctx: Typectx.ctx) =
+  let ht = Hashtbl.create (List.length ctx) in
+  let add (name: id) =
+    let new_name = Rename.unique name in
+    (* TODO: remove this since context addition checks this already ?*)
+    if Hashtbl.mem ht name then failwith "duplicate key"; 
+    Hashtbl.add ht name new_name;
+    new_name
+  in
+  (List.map (map_fst add) ctx, ht)
+
+let ctx_union_r (l: Typectx.ctx) (r: Typectx.ctx) =
+  map_fst (fun res -> l @ res) (freshen r)
+
+(* TODO `ctx_subst` is wrong.
+  We need to substitute occurrences of context variables in
+  the propositions they appear in. *)
+let ctx_subst (ctx: (id * UT.t) list) (ht: (id, id) Hashtbl.t) =
+  List.map (fun (name, ty) ->
+    match Hashtbl.find_opt ht name with
+    | Some new_name -> (new_name, UT.subst_id ty name new_name)
+    | None -> (name, ty)) ctx
+
+let mk_app (f_id: id NNtyped.typed) (args: id NNtyped.typed list) k =
+  let name = Rename.name () in
+  let resty =  snd (NT.destruct_arrow_tp (snd f_id.ty)) in
+  let resid: id NNtyped.typed = {x = name; ty = (None, resty)} in
+  let args = List.map NL.id_to_value args in
+  NL.LetApp {
+    ret = resid;
+    f = f_id ;
+    args = args;
+    body = k resid
+  }
+
+let mk_ctor (ctor: id NNtyped.typed) (args: id NNtyped.typed list) k =
+  let name = Rename.name () in
+  let resty =  snd (NT.destruct_arrow_tp (snd ctor.ty)) in
+  let resid: id NNtyped.typed = {x = name; ty = (None, resty)} in
+  let args = List.map NL.id_to_value args in
+  NL.LetDtConstructor {
+    ret = resid;
+    f = ctor ;
+    args = args;
+    body = k resid
+  }
+
+let mk_op (op_id: Op.op) (args: id NNtyped.typed list) =
+  let op_ty = Abstraction.Prim.get_primitive_normal_ty (Op.op_to_string op_id)  in
+  let name = Rename.name () in
+  let resty =  snd (NT.destruct_arrow_tp op_ty) in
+  let resid: id NNtyped.typed = {x = name; ty = (None, resty)} in
+  let args = List.map NL.id_to_value args in
+  NL.LetOp {
+    ret = resid;
+    op = op_id ;
+    args = args;
+    body = NL.value_to_term (NL.id_to_value resid)
+  }
+
+
+(* let tmp = Term2normalanormal.to_anormal {ty = Some (None, Ty_unit); x = (App ({ty = Some (None, Ty_constructor ("x", [])); x = Var "tt"}, []))} None
+let _ = dbg_sexp (NNtyped.sexp_of_typed NL.sexp_of_term tmp)
+let unit_ctor: id NNtyped.typed = {x = "tt"; ty = (None, Ty_unit)}
+let _ = exit 0 *)
+
+(* Example below shows how to build a term and call inference on it *)
+let example_term = fun () ->
+  let int_gen = List.nth libs 2 in
+
+  let t_int_gen: id NL.typed = { x = (fst int_gen); ty = (None, Underty.T.erase (snd int_gen)) } in
+
+  let four = (NL.V {x = (NL.Lit (NL.ConstI 4)); ty = (None, Ty_int) }) in
+  let prog =
+    mk_ctor {x = "tt"; ty = (None, Ty_unit)} [] (fun v -> 
+      {x = mk_app t_int_gen [v] (fun v -> NL.value_to_term (NL.id_to_value v)); ty = (None, Ty_int) }) in
+  let res = Typecheck.Undercheck.term_type_infer
+    { nctx; ctx = Typectx.empty; libctx }
+    ({x = prog; ty = (None, Ty_int)}) in
+
+  print_endline "Typed int_gen example";
+  dbg res;
+  dbg_sexp (NL.sexp_of_term prog);
+  ()
+
+let _ = example_term ()
 
 let results =
   List.mapi
@@ -159,10 +268,7 @@ let results =
           let retty = UT.subst_id retty argname decreasing_arg.x in
           let lambody = NL.subst_id (fstarg.x, decreasing_arg.x) lambody in
 
-          let formatter = Format.formatter_of_out_channel Out_channel.stdout in
-          let () = Core.Sexp.pp_hum formatter (UT.sexp_of_t retty) in
-          let () = Format.pp_print_flush formatter () in
-
+          dbg retty;
           print_endline "\n\n===\n";
 
           let res =
@@ -177,9 +283,4 @@ let results =
           res) (* List.mapi *)
     refinements
 
-(* let () = List.iter (fun x -> print_endline (string_of_bool x)) results *)
-
-let results = List.map UT.sexp_of_t results
-let formatter = Format.formatter_of_out_channel Out_channel.stdout
-let () = List.iter (Core.Sexp.pp_hum formatter) results
-let () = Format.pp_print_flush formatter ()
+let () = List.iter (fun x -> dbg x) results
