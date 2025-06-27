b945d882b659511e12f6fcb0c4edb4d5846de5da
Patrick LaFontaine <32135464+Pat-Lafon@users.noreply.github.com>
2025-06-26 22:51:57 -0700

    synth.py no longer multi-threaded

--- scripts/synth.py
+++ scripts/synth.py
@@ -1,6 +1,5 @@
 import os
 import re
-
 from runner import invoc_cmd, run_for_dir_from_cli, rescode_non_zero
 
 cmd_prefix = "dune exec Cobb --no-buffer -- synthesis".split(" ")
@@ -8,10 +7,8 @@ config_file = "meta-config.json"
 working_dir = "underapproximation_type"
 verbose = True
 
-
 def run_synthesis_aux(cmd):
     invoc_cmd(verbose, cmd, rescode_non_zero, None, cwd=None)
-
 
 def run_synthesis(dir_str: str):
     meta_config_file = "{}/{}".format(dir_str, config_file)
@@ -21,21 +18,12 @@ def run_synthesis(dir_str: str):
             if os.path.isdir(pp):
                 run_synthesis(pp)
     else:
-        from multiprocessing import Pool
-
-        pool = Pool()
-
-        multiple_res = []
         for filename in os.listdir(dir_str):
             matches = re.search(r"prog[0-9]+\.ml$", filename, re.MULTILINE)
             if matches:
                 filename = dir_str + "/" + filename
                 cmd = cmd_prefix + [filename]
-                res = pool.apply_async(run_synthesis_aux, args=[cmd])
-                multiple_res.append(res)
-
-        [res.get() for res in multiple_res]
-
+                run_synthesis_aux(cmd)
 
 if __name__ == "__main__":
     run_for_dir_from_cli(working_dir, run_synthesis)
