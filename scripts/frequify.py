d42358ef9bb2590015a0a5bf251b0d9137771fa4
Francille <11zhuangfz@gmail.com>
2025-07-29 19:55:26 -0400

    update frequify + half solution that doesn't work for postprocess

--- scripts/frequify.py
+++ scripts/frequify.py
@@ -2,11 +2,8 @@ import subprocess
 import os
 import re
 from pathlib import Path
-# dir : underapproximation_type/data/validation/_/prog.ml
-# cmd : dune exec frequify -- -f freq_gen [dir]
-# everything is freq_gen excpet for rbtree
+# cmd : dune exec frequify -- -f [freq] -o [out] [dir]
 
-cmd_prefix = "dune exec frequify -- -f".split(" ")
 in_dir_str = "./underapproximation_type/data/validation/"
 out_dir_str = "../Cobb_PBT/bin/"
 
@@ -22,17 +19,15 @@ folder_names = {
     "uniquelist":"unique_list",
 }
 
-subprocess.run(cmd_prefix + ["-f"] + ["freq_gen"] + ["-o"] + ["../Cobb_PBT/bin/sized_list/prog1_syn.ml"]
-+ ["./underapproximation_type/data/validation/sizedlist/prog1.ml.syn"])
-
 in_dir = Path(in_dir_str)
 assert in_dir.is_dir()
 
 # prog*.ml
 for d in (os.listdir(in_dir)):
     gen_d = f"{in_dir}/{d}"
+    match = re.search(r"(?:_excess)|(?:_imprecise)|(?:_[123]){1}|(?:_size)|(?:proofs)$", d)
 
-    if os.path.isdir(gen_d):
+    if (os.path.isdir(gen_d) and not match):
         is_stlc = re.search(r"stlc", d)
         if not (is_stlc):
             for f in os.listdir(gen_d):
@@ -45,13 +40,11 @@ for d in (os.listdir(in_dir)):
                     else:
                         out_file = f"{out_dir_str}{d}/{base}_syn{ext}"
                     
-                    # print(f"i = {in_file}   o = {out_file}")
+                    print(f"i = {in_file}   o = {out_file}")
 
                     if d == "rbtree":
-                        cmd = cmd_prefix + ["unif_gen"] + ["-o"] + [out_file] + [in_file]
+                        cmd = f"dune exec frequify -- -f unif_gen -o {out_file} {in_file}".split(" ")
                     else:
-                        cmd = cmd_prefix + ["freq_gen"] + ["-o"] + [out_file] + [in_file]
+                        cmd = f"dune exec frequify -- -f freq_gen -o {out_file} {in_file}".split(" ")
                     subprocess.run(cmd)
-
-                    # print(file)
             
